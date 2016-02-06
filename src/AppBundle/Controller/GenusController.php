8e1671894e031d68f5e9b2eed62c87b23c05b467
Walter Moore <walteragnewmoore@gmail.com>
2016-02-06 17:48:15 -0600

    listing & using services, twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -13,9 +13,14 @@ class GenusController extends Controller
     */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig',[
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ]
+        return $this->render('genus/show.html.twig', [
             'name' => $genusName
+            'notes' => $notes
         ]);
         
         return new Response($html);
